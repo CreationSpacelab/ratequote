f4546402d2a3a0a8d828821ff1a2b9c2b70fdf8d
creationspacelab <creationspacelab@gmail.com>
2020-12-07 15:31:24 -0500

    Connect redux to application and set RateQuoteUI

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RateQuoteUI from "./RateQuoteUI"
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux"
+
+import { GlobalStyles } from "./components/core/globalStyles";
+import RateQuoteUI from "./components/RateQuoteUI/RateQuoteUI";
+import reportWebVitals from "./reportWebVitals";
+import store from "./store"
 
 ReactDOM.render(
   <React.StrictMode>
-    <RateQuoteUI />
+    <Provider store={store}>
+      <GlobalStyles />
+      <RateQuoteUI />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
